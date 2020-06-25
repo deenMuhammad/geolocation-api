483ea97d7cf4eba89d24dabf57b0f3399d1d4f61
zorro199696@gmail.com <mac@macui-MacBook-Pro.local>
2020-06-25 23:37:13 +0900

    new push

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,18 @@
-var mysql      = require('mysql');
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'geolocation'
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "geolocation",
 });
- 
-connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
 });
- 
-connection.end();
+
+module.exports = connection;
